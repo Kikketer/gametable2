7ce2b909324a0081f04954faff5ca480c9ab9889
Chris Weed <chris@cjweed.com>
2023-05-30 22:46:07 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container p-2 mx-auto my-3 border-2 rounded-md">
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+export default App;
