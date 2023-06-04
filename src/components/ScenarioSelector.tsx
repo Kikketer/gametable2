160a0091c3735cb7b8996ed9150649e55a510c70
Chris Weed <chris@cjweed.com>
2023-06-04 11:34:25 -0500

    🔥

--- src/components/ScenarioSelector.tsx
+++ src/components/ScenarioSelector.tsx
@@ -6,28 +6,35 @@ export const ScenarioSelector = (): ReactElement => {
   const { currentScenario, setCurrentScenario, scenarios } = useGameBoard()
 
   const selectScenario = (folderName: Scenario['folderName']) => {
-    const scenario = scenarios?.find(
-      (scenario) => scenario.folderName === folderName
-    )
-    if (scenario) {
-      setCurrentScenario(scenario)
-    }
+    console.log('Event ', folderName)
+    // const scenario = scenarios?.find(
+    //   (scenario) => scenario.folderName === folderName
+    // )
+    // if (scenario) {
+    //   setCurrentScenario(scenario)
+    // }
   }
 
   return (
     <div className="flex gap-3">
       <label htmlFor="scenario-selector">Pick a scenario:</label>
-      <select
+      <input
+        type="file"
         id="scenario-selector"
-        value={currentScenario?.folderName}
-        onSelect={(e) => selectScenario(e.currentTarget.value)}
-      >
-        {scenarios?.map((scenario) => (
-          <option key={scenario.folderName} value={scenario.folderName}>
-            {scenario.name}
-          </option>
-        ))}
-      </select>
+        accept=".json"
+        onChange={(e) => selectScenario(e.target.value)}
+      />
+      {/*<select*/}
+      {/*  id="scenario-selector"*/}
+      {/*  value={currentScenario?.folderName}*/}
+      {/*  onSelect={(e) => selectScenario(e.currentTarget.value)}*/}
+      {/*>*/}
+      {/*  {scenarios?.map((scenario) => (*/}
+      {/*    <option key={scenario.folderName} value={scenario.folderName}>*/}
+      {/*      {scenario.name}*/}
+      {/*    </option>*/}
+      {/*  ))}*/}
+      {/*</select>*/}
     </div>
   )
 }
