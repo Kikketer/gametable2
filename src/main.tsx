10dd494f3eefa3ee225e1aa9e9f72850d6e84c28
Chris Weed <chris@cjweed.com>
2023-06-17 10:19:47 -0500

    Dialog is working

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { CommonWindow } from './CommonWindow.tsx'
-import { NoDice } from './routes/NoDice.tsx'
-import { Component as Controller } from './routes/Controller.tsx'
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-const router = createBrowserRouter([
-  {
-    element: <CommonWindow />,
-    errorElement: <NoDice />,
-    children: [
-      {
-        path: '/controller',
-        element: <Controller />,
-        // lazy: () => import('./routes/Controller'),
-      },
-    ],
-  },
-])
+const root = createRoot(document.getElementById("app"));
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <div className="container p-2 mx-auto my-3 border-2 rounded-md">
-      <RouterProvider router={router} />
-    </div>
-  </React.StrictMode>
-)
+root.render(<App />);
